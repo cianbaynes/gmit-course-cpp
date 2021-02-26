51223cd01fff0fe6a638ee02c7d4ab2e42ca2ff4
Cian Baynes <cianbaynes@gmail.com>
2021-02-26 13:17:46 +0000

    lap-program-club for submission

--- lab-program-club/football_club/main.cpp
+++ lab-program-club/football_club/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 #include "football_club.h"
 #include "player.h"
@@ -14,10 +14,10 @@ int main()
     cout <<"Clubname: "<< footballClub1.getClubname() <<", District: " << footballClub1.getDistrict()<< ", Strip Colour: " << footballClub2.getStripColour()<< endl;
 
  Player Player1("Cian", "Baynes");
- Player Player2("Cian", "Baynes", 12345678, "Winger");
+ Player Player2("Cian", "Baynes", 876330989, "Winger");
 
     cout<<"Forename: "<<Player1.getForename()<<", Surname: "<<Player1.getSurname()<<endl;
-    cout<<"Forename: "<<Player1.getForename()<<", Surname: "<<Player1.getSurname()<<", Phone Number: "<<Player2.getMobileNumber()<<", Position:"<<Player2.getPosition()<<endl;
+    cout<<"Forename: "<<Player1.getForename()<<", Surname: "<<Player1.getSurname()<<", Phone Number: "<<setw(10)<<setfill('0')<<Player2.getMobileNumber()<<", Position:"<<Player2.getPosition()<<endl;
 
 return 0;
 }
